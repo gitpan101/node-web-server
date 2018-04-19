29331b70268919fe256e758ed8b26a1ee0b1c487
Ashutosh <ashpan101@gmail.com>
2018-04-19 16:05:24 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to file');
+        }
+    });
+
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: '404 Bad Request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
